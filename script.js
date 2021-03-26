209898e94e4256c3393a949cc82c215cdbf846b2
gagandeep.ahuja <gagandeepahuja09@gmail.com>
2021-03-26 21:01:29 +0530

    add: higher order functions

--- script.js
+++ script.js
@@ -24,38 +24,53 @@
 // createBooking('LH123', 2)
 // createBooking('LH123', undefined, 1000)
 
-const flight = 'LH234'
-const me = {
-  name: 'Gagandeep',
-  passport: 245464521
-}
+// PART 2: Passing reference and primitive parameters
+// const flight = 'LH234'
+// const me = {
+//   name: 'Gagandeep',
+//   passport: 245464521
+// }
 
-const checkIn = (flightNum, passenger) => {
-  flightNum = 'LH999'
-  passenger.name = `Mr. ${passenger.name}`
-  if (passenger.passport === 245464521) {
-    console.log('YO')
-  } else {
-    console.log('NO')
-  }
-}
+// const checkIn = (flightNum, passenger) => {
+//   flightNum = 'LH999'
+//   passenger.name = `Mr. ${passenger.name}`
+//   if (passenger.passport === 245464521) {
+//     console.log('YO')
+//   } else {
+//     console.log('NO')
+//   }
+// }
 
-// It's the same as doing
-// const flightNum = flight
-// const passenger = me
-// When we log me, we'll get the change as passed by reference
-// But not with flight as it's a copy(parameter type)
+// // It's the same as doing
+// // const flightNum = flight
+// // const passenger = me
+// // When we log me, we'll get the change as passed by reference
+// // But not with flight as it's a copy(parameter type)
 
-checkIn(flight, me)
+// checkIn(flight, me)
 
 
-// Interaction of 2 or more different functions with the same object can cause issues.
+// // Interaction of 2 or more different functions with the same object can cause issues.
 
-const newPassport = person => {
-  person.passport = Math.random(Math.random() * 100000000000)
-}
+// const newPassport = person => {
+//   person.passport = Math.random(Math.random() * 100000000000)
+// }
 
-newPassport(me)
-checkIn(flight, me)
+// newPassport(me)
+// checkIn(flight, me)
 
-// Note: We can pass a reference variable in JS. But this is not the same as passing by reference.
+// // Note: We can pass a reference variable in JS. But this is not the same as passing by reference.
+
+
+
+// PART 3: First Class And Higher Order Functions
+// Functions as first class citizens ===> Just another type of object.
+// 1. Store functions in variables or object properties.
+// 2. Pass methods as arguments to other functions
+// 3. Return functions from other functions.
+// 4. Call methods on other functions. ==> counter.inc.bind(someOtherObject)
+// counter.inc is the function and we are calling bind on that function.
+
+// Higher order functions: Function that receives another function as an argument,
+// that returns a new function or both.
+// eg. addEventListener
